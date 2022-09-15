153a1a3d7bfcc60eaf305c46178ac3d6d5c7baaa
kasys1422 <ka.sys.1422@gmail.com>
2022-09-15 18:40:28 +0900

    Update wasm2base64module.js

--- wasm2base64module.js
+++ wasm2base64module.js
@@ -1,3 +1,39 @@
+export function arrayBufferToBase64(array) {
+    let binary = "";
+    let bytes = new Uint8Array(array);
+    for (let i = 0; i < bytes.byteLength; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
+export function wasmBinaryArray2Base64(wasm_binary, compress = null){
+    let array = new Uint8Array(wasm_binary);
+    return new Promise(function (resolve) {
+        if(typeof compress == "string" && compress != "none" && compress != "" && typeof CompressionStream == "function"){
+            new Response(new Blob([array]).stream().pipeThrough(new CompressionStream(compress))).arrayBuffer().then(function(array_buf) {
+                resolve(arrayBufferToBase64(array_buf)); 
+            });
+        }else{
+            new Response(new Blob([array])).arrayBuffer().then(function(array_buf) {
+                resolve(arrayBufferToBase64(array_buf)); 
+            });
+        }
+    })
+}
+
+export function wasmBinaryUrl2Base64(url, compress = null){
+    return new Promise(function (resolve) {
+        fetch(url).then(function(result) {
+            return result.arrayBuffer();
+        }).then(function(result) {
+            wasmBinaryArray2Base64(result, compress)
+        }).then(function(result) {
+            resolve(result);
+        });
+    })
+}
+
 export function base64Wasm2WasmBinary(wasm_base64, compress = null){
     let array = Uint8Array.from(Array.prototype.map.call(atob(wasm_base64), (x) => {return x.charCodeAt(0)}));
     if(typeof compress == "string" && compress != "none" &&  compress != "" && typeof DecompressionStream == "function"){
